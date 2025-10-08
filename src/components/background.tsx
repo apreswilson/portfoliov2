e4bf40398cc11e41defad4f112efceb1ca9a4be1
Alex Wilson <apreswilson@gmail.com>
2025-10-08 19:43:28 -0400

    Mobile loading background image fix 2

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,7 +1,7 @@
 export default function Background() {
   return (
     <div className="fixed inset-0 h-[100dvh] w-full">
-      <div className="absolute inset-0 bg-[url('/bg.jpg')] bg-cover bg-center"></div>
+      <div className="absolute inset-0 bg-[url('/bg.jpg')] bg-cover bg-center transform-gpu"></div>
       <div className="absolute inset-0 bg-black/10 backdrop-blur-[4px]"></div>
       <p className="absolute inset-0 top-[calc(100dvh-1rem)] lg:top-[calc(100dvh-2rem)] left-2 text-white/90 font-light text-xs lg:text-base select-none italic">
         Credit @aenamiart
